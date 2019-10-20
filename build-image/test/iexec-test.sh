50f21f6a577fc92842606a2d5cbe918548908c6d
maggo <marcokaul@gmail.com>
2019-10-20 13:27:54 +0200

    Update test script

--- build-image/test/iexec-test.sh
+++ build-image/test/iexec-test.sh
@@ -4,8 +4,8 @@ BASE_PATH=$(pwd)
 
 DIR_IN=`pwd`/iexec_in
 DIR_OUT=`pwd`/iexec_out
-DOCKERIMAGE=979879a2ab36
-CMDLINE='https://github.com/maggo/mar.co.de.git dist yarn run build'
+DOCKERIMAGE=maggo/decentralify:v0.1
+CMDLINE='https://github.com/maggo/decentralify.git build yarn run build'
 
 docker run --rm \
   -v ${DIR_IN}:/iexec_in \
